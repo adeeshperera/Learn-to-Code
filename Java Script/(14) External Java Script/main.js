2cedfb589069a7b48452bce451c7fc9135226895
adeeshperera <user.adeeshperera@hotmail.com>
2024-05-25 09:06:51 -0700

    External Java Script

---
+++ Java Script/(14) External Java Script/main.js
@@ -0,0 +1,9 @@
+document.getElementById("button1").onclick= function(){
+    document.getElementById("test01").style.color= "red";
+}
+document.getElementById("button2").onclick= function(){
+    document.getElementById("test01").style.fontSize= "50px";
+}
+document.getElementById("button3").onclick= function(){
+    document.getElementById("test01").style.display= "none";
+}
