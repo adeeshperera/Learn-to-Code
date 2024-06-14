50ec72f6fb05af220078a984997ce7ef377b9a97
adeeshperera <user.adeeshperera@hotmail.com>
2024-06-14 05:43:30 -0700

    Modules

---
+++ Java Script/(33) Modules/mathUtils.js
@@ -0,0 +1,16 @@
+// File: mathUtils.js
+// This is a module containing utility functions for mathematical operations.
+
+// Exporting a named function as the default export
+export default function add(a, b) {
+    return a + b;
+}
+
+// Exporting a named function
+export function multiply(a, b) {
+    return a * b;
+}
+
+// Exporting a named constant
+export const PI = 3.14159;
+
