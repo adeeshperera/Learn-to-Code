1e5e11c459b6cb8d18e14766eba87e29381fafb9
adeeshperera <user.adeeshperera@hotmail.com>
2024-07-12 08:47:11 -0700

    Hello-World

---
+++ PHP/(01)-Hello-World.php
@@ -0,0 +1,18 @@
+<?php
+echo "Hello World!<br>";
+ECHO "Hello World!<br>";
+EcHo "Hello World!<br>";
+#In PHP, keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are not case-sensitive
+
+echo "This ", "string ", "was ", "made ", "with multiple parameters.<br>";
+
+echo "<br>";
+echo 10 * 10;
+
+// This is a single-line comment
+
+# This is also a single-line comment
+
+/* This is a
+multi-line comment */
+?>
