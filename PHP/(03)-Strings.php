d17571a2ecbbc33bef771910c2dcaa4b78eb9e3c
adeeshperera <user.adeeshperera@hotmail.com>
2024-07-13 06:06:31 -0700

    Strings

---
+++ PHP/(03)-Strings.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello";
+echo 'Hello';
+echo "<br>";
+
+$x = "John";
+echo "Hello $x";
+echo "<br>";
+
+$y = "John";
+echo 'Hello $y';
+echo "<br>";
+
+$txt = "Test";
+echo "Test Text " . $txt . "!";
+?>
+
