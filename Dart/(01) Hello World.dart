9e1602774719f7da1f9468e79f724239f2b077f6
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-03 08:42:59 -0700

    Hello World

---
+++ Dart/(01) Hello World.dart
@@ -0,0 +1,7 @@
+// Dart program to print Hello, World
+void main() {
+  // Main function is the entry point of Dart programs
+
+  // Printing Hello, World to the console
+  print('Hello, World!');
+}
