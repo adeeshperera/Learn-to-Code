09e7c6ff7bf0595e88d20c5045ca72caee7c4c06
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-14 09:43:51 -0700

    Comments

---
+++ C#/(02.) Comments/(02.) Comments/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.__Comments
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+            //Test single-line comment
+            /*Multiline comments*/
+        }
+    }
+}
