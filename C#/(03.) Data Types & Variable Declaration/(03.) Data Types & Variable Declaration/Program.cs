37f5690f4dcb2f843c86fac04e83fa4296a31ef9
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-16 10:59:45 -0700

    Data Types & Variable Declaration

---
+++ C#/(03.) Data Types & Variable Declaration/(03.) Data Types & Variable Declaration/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.__Data_Types___Variable_Declaration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testVar1;            // String
+            int testVar2;           // Integer (whole number)
+            float testVar3;         // Floating point number
+            double testVar4;            // Floating point number
+            char testVar5;          // Character
+            bool testVar6;          // Boolean
+
+            Console.ReadLine();
+        }
+    }
+}
