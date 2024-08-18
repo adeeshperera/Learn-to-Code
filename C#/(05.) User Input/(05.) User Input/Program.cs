a2a0980f17f94c8966c25d6f0c32fc9d71927cc9
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-18 05:18:14 -0700

    User Input

---
+++ C#/(05.) User Input/(05.) User Input/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.__User_Input
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string testVar1;
+            Console.Write("Test input : ");
+            testVar1 = Console.ReadLine();
+            Console.WriteLine("The test input is " + testVar1);
+            Console.WriteLine("\n");
+
+            Console.Write("Test number input : ");
+            int testVar2 = Int16.Parse(Console.ReadLine());
+            /*
+            int testVar2;
+            testVar2 = Int16.Parse(Console.ReadLine());
+            */
+            Console.WriteLine("The test number input is " + testVar2);
+            Console.ReadLine();
+        }
+    }
+}
