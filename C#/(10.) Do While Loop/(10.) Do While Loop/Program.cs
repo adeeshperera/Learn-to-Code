dde7e9b936cadce14ce1939f5f5c92c0e1b084ac
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-23 05:37:47 -0700

    Do While Loop

---
+++ C#/(10.) Do While Loop/(10.) Do While Loop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.__Do_While_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testVar1;
+            double testVar2 = 0;            //Total
+            do
+            {
+                Console.Write("Test Input: ");
+                testVar1 = Int32.Parse(Console.ReadLine());
+                testVar2 += testVar1;           //testVar2 = testVar2 + testVar1;
+            } while (testVar1 != 0);
+            Console.WriteLine("Total= " + testVar2);
+            Console.WriteLine("\n");
+
+            int testVar3;
+            double testVar4 = 0;            // Total
+            do
+            {
+                Console.Write("Test Input: ");
+                testVar3 = Int32.Parse(Console.ReadLine());
+
+                if (testVar3 == -1)
+                {
+                    break;
+                }
+                testVar4 += testVar3;
+            } while (true);
+
+            Console.WriteLine("Total = " + testVar4);
+            Console.ReadLine();
+        }
+    }
+}
