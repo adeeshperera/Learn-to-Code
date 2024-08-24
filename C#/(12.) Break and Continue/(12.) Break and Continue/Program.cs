0d3de731f32c01ca5188026c63c3ba6ab1ff3082
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-24 09:11:58 -0700

    Break and Continue

---
+++ C#/(12.) Break and Continue/(12.) Break and Continue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.__Break_and_Continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 5; i++)
+            {
+                if (i == 3)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("\n");
+
+            for (int i = 5; i >= 0; i--)
+            {
+                if (i == 3)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
