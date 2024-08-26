36884922a0a1ff8a2c9ee27e6f0c76903eff11d7
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-26 08:51:26 -0700

    Arrays

---
+++ C#/(13.) Arrays/(13.) Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.__Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testVar1;
+            testVar1 = new int[5];          //int[] testVar1 = new int[5];
+            for(int i = 0; i < 5; i++)          //i = Index
+            {
+                Console.Write("Test input " + i + "- ");
+                testVar1[i] = Int32.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine(testVar1[3]);
+            Console.WriteLine("\n");
+
+            for(int a = 0; a <5; a++)
+            {
+                Console.WriteLine(testVar1[a]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
