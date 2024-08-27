5bc1165f7c8aa0a805aaad666b03c0060b9e5557
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-27 09:39:05 -0700

    Function - using no parameter and return type

---
+++ C#/(14.) Function - using no parameter and return type/(14.) Function - using no parameter and return type/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.__Function___using_no_parameter_and_return_type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            testFunction();
+            testFunction();
+            testFunction();
+            Console.ReadLine();
+        }
+        static void testFunction()
+        {
+            Console.WriteLine("Test text");
+            //A function that does not return any value specifies void type as a return type
+        }
+    }
+}
