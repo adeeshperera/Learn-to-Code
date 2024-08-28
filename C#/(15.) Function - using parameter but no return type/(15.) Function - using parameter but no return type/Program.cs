a006c0835e4aff5d6d56cc3b3f869529102e4267
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-28 06:24:31 -0700

    Function - using parameter but no return type

---
+++ C#/(15.) Function - using parameter but no return type/(15.) Function - using parameter but no return type/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.__Function___using_parameter_but_no_return_type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            testFunction("input 1");
+            testFunction("input 2");
+            Console.ReadLine();
+        }
+        static void testFunction(string testParameter)
+        {
+            Console.WriteLine("Test " + testParameter);
+            //A function can have zero or any number of parameters to get data
+        }
+    }
+}
