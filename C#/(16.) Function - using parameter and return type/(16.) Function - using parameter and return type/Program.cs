4c6a49938618d8e7074d225a0c2b0037ee8ff68b
adeeshperera <user.adeeshperera@hotmail.com>
2024-08-29 06:13:07 -0700

    Function - using parameter and return type

---
+++ C#/(16.) Function - using parameter and return type/(16.) Function - using parameter and return type/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.__Function___using_parameter_and_return_type
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(testFunction(10, 20));
+            Console.ReadLine();
+        }
+        static int testFunction(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
