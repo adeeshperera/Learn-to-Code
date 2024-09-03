23ccec521bd1b14e522b692f8bdc7c1955de70a8
adeeshperera <user.adeeshperera@hotmail.com>
2024-09-03 09:11:42 -0700

    Methods-as-props

---
+++ React/React for Beginners/(04)-Methods-as-props/methods-as-props/src/App.js
@@ -0,0 +1,12 @@
+import ParentComponent from "./components/ParentComponent";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<ParentComponent />
+		</div>
+	);
+}
+
+export default App;
