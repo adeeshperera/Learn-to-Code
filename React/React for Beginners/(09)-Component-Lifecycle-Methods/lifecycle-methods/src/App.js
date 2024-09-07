f2ad2493316a12fc9f9d1ae28f17a479b291bd86
adeeshperera <user.adeeshperera@hotmail.com>
2024-09-07 11:39:33 -0700

    Component-Lifecycle-Methods

---
+++ React/React for Beginners/(09)-Component-Lifecycle-Methods/lifecycle-methods/src/App.js
@@ -0,0 +1,12 @@
+import LifecycleA from "./components/LifecycleA";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<LifecycleA />
+		</div>
+	);
+}
+
+export default App;
