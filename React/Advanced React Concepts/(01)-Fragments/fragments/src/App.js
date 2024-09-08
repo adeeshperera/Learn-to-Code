31387ab7e6de6a09de293cbfe2d09792a210e06f
adeeshperera <user.adeeshperera@hotmail.com>
2024-09-08 04:49:31 -0700

    Fragments

---
+++ React/Advanced React Concepts/(01)-Fragments/fragments/src/App.js
@@ -0,0 +1,14 @@
+import FragmentDemo from "./components/FragmentDemo";
+import Table from "./components/Table";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<FragmentDemo />
+			<Table />
+		</div>
+	);
+}
+
+export default App;
---
+++ React/Advanced React Concepts/(01)-Fragments/fragments/src/components/Columns.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Columns() {
+	return (
+		// It can accept the key attribute when rendering lists of items Eg.- <React.Fragment key={item.id}>
+		<>
+			{/* <React.Fragment> = <> */}
+			<td>Name</td>
+			<td>Vishwas</td>
+		</>
+	);
+}
+
+export default Columns;
