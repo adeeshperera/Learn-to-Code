a821052bf027916ef519581ff3f898a5ebfff697
adeeshperera <user.adeeshperera@hotmail.com>
2024-09-11 11:16:15 -0700

    Portals

---
+++ React/Advanced React Concepts/(04)-Portals/portals/src/App.js
@@ -0,0 +1,12 @@
+import PortalDemo from "./components/PortalDemo";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<PortalDemo />
+		</div>
+	);
+}
+
+export default App;
