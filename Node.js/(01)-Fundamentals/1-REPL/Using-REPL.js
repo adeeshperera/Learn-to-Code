2f7069dc1189b875df492108d6867ad2bc2ebd2e
adeeshperera <user.adeeshperera@hotmail.com>
2024-09-19 03:55:05 -0700

    Fundamentals

---
+++ Node.js/(01)-Fundamentals/1-REPL/Using-REPL.js
@@ -0,0 +1,25 @@
+// $ node                               //=> CLI
+
+// Welcome to Node.js v20.12.2.
+// Type ".help" for more information.
+
+// > console.log('Hello, World!');      //=> Read & Evaluate
+// Hello, World!                        //=> Print
+// undefined                            //=> Loop (Return value)
+
+// > let x = 'test';
+// undefined                            //=> Loop
+
+// > x
+// 'test'
+
+// > x + ' text'
+// 'test text'
+
+// > function test (a, b) {
+// ... return a + b;
+// ... }
+// undefined                            //=> Loop
+
+// > test(1, 2)
+// 3
