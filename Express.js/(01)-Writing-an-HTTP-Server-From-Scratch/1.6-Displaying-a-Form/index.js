abf2692eb0d2bc74c2a0a0270cb41b7d2ebbbac0
adeeshperera <user.adeeshperera@hotmail.com>
2024-11-07 07:19:07 -0800

    Displaying-a-Form

---
+++ Express.js/(01)-Writing-an-HTTP-Server-From-Scratch/1.6-Displaying-a-Form/index.js
@@ -0,0 +1,56 @@
+import { createServer } from "http";
+import guitars from "./data.js";
+import { createList, getForm, getGuitarContent, view } from "./content.js";
+
+const server = createServer((request, response) => {
+	// /delete/id
+	// /add
+	const parts = request.url.split("/");
+
+	// If the url contains the words 'delete'
+	if (parts.includes("delete")) {
+		handleDelete(parts[2]);
+		redirect(response, "/");
+	} else {
+		response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+
+		const url = new URL(request.url, "http://localhost");
+
+		const id = url.searchParams.get("id");
+
+		let content = "";
+
+		// If the url contains the words 'add'
+		if (parts.includes("add")) {
+			content = getForm(); // Call the getForm function
+		} else if (id) {
+			// Display the guitar using ID
+			let guitar = guitars.find((g) => g.id == id);
+			content = getGuitarContent(guitar);
+		} else {
+			// Displaying all existing guitars
+			content = createList(guitars);
+		}
+
+		response.end(view(content)); // Displaying the final content
+	}
+});
+
+function handleDelete(id) {
+	let index = guitars.findIndex((g) => g.id == id);
+
+	// TODO: check index
+
+	guitars.splice(index, 1);
+}
+
+function redirect(response, to) {
+	response.writeHead(302, { location: to, "Content-Type": "text/plain" });
+	response.end(`Redirect to ${to}`);
+}
+
+server.listen(80, () => {
+	console.log(
+		`Server is listening at http://localhost:${server.address().port}`
+	);
+});
