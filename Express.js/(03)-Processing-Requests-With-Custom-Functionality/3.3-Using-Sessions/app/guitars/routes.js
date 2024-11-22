280c436c274e73c296bc0df2c70b73ec54828f20
adeeshperera <user.adeeshperera@hotmail.com>
2024-11-22 09:51:33 -0800

    Using-Sessions

---
+++ Express.js/(03)-Processing-Requests-With-Custom-Functionality/3.3-Using-Sessions/app/guitars/routes.js
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import {
+	createGuitar,
+	deleteGuitar,
+	editGuitar,
+	listGuitars,
+	showGuitar,
+	storeGuitar,
+	updateGuitar,
+} from "./controller.js";
+import { checkAuth } from "../auth/controller.js"; // Check if a user is logged in
+export const routes = new Router();
+
+// /guitars
+
+routes.get("/", listGuitars);
+routes.post("/", checkAuth, storeGuitar);
+routes.get("/create", checkAuth, createGuitar);
+routes.get("/:id/edit", checkAuth, editGuitar);
+routes.get("/:id/delete", checkAuth, deleteGuitar);
+
+routes.get("/:id", showGuitar);
+routes.post("/:id", checkAuth, updateGuitar);
