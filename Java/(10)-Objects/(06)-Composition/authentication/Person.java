0176763112ae4b7379b9357d197dabcf4c03c71c
adeeshperera <user.adeeshperera@hotmail.com>
2025-03-18 23:33:49 -0800

    Create Person.java

---
+++ Java/(10)-Objects/(06)-Composition/authentication/Person.java
@@ -0,0 +1,6 @@
+package authentication;
+
+// The Person interface requires implementing classes to provide a method for getting the person's name.
+public interface Person {
+    String getName();
+}
