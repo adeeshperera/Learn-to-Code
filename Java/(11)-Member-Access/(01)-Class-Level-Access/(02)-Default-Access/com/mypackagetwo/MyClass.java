593ed642fffcd094052691365388bd67f1441c92
adeeshperera <user.adeeshperera@hotmail.com>
2025-03-26 03:22:58 -0800

    Create MyClass.java

---
+++ Java/(11)-Member-Access/(01)-Class-Level-Access/(02)-Default-Access/com/mypackagetwo/MyClass.java
@@ -0,0 +1,9 @@
+package com.mypackagetwo; // Package declaration
+
+// Class has default access (no public keyword), so it is accessible only within this package
+class MyClass {
+    // Public method that returns a greeting message
+    public String sayHello() {
+        return "hello";
+    }
+}
