8000e693bf8eb6401b073bf6d34c11ce34f92883
adeeshperera <user.adeeshperera@hotmail.com>
2025-04-26 02:35:27 -1000

    Create Cheetah.java

---
+++ Java/(12)-Object-Oriented-Concepts/(03)-Polymorphism/polymorphism/Cheetah.java
@@ -0,0 +1,9 @@
+package polymorphism;
+
+// Cheetah class implementing LandAnimal
+public class Cheetah extends LandAnimal {
+    @Override
+    public void run() {
+        System.out.println("Cheetah runs very fast.");
+    }
+}
